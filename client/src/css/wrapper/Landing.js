ac73c43ee780180eb97b53d582cf7eca7d7a1070
Amogh_Garg <amoghgarg9@gmail.com>
2023-02-12 18:00:33 +0530

    Post component added

--- client/src/css/wrapper/Landing.js
+++ client/src/css/wrapper/Landing.js
@@ -1,44 +1,42 @@
-
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.main`
-nav{
-    width : var(--fluid-width);
-    max-width : var(--max-width);
-    margin : 0 auto;
-    height : var(--nav-height);
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
     display: flex;
     align-items: center;
-} 
+  }
 
-.page{
+  .page {
     min-height: calc(100vh- var(--nav-height));
     display: grid;
     align-items: center;
-    margin-top : -3rem;
-}
+    margin-top: -3rem;
+  }
 
-h1{
+  h1 {
     font-weight: 700;
-    span{
-        color: var(--primary-500);
+    span {
+      color: var(--primary-500);
     }
-}
-.main-img{
-    display:none;
-}
+  }
+  .main-img {
+    display: none;
+  }
 
-@media (min-width: 992px){
-    .page{
-        grid-template-columns: 1fr 1fr;
-        column-gap:3rem ;
-    }
-
-    .main-img{
-        display: block;
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
     }
 
-}
-`
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Wrapper;
